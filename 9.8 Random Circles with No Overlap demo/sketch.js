f472a13c9dc4fa14c4d0294e1acb3a948bd64787
zx122248006 <zx122248006@qq.com>
2019-09-14 20:50:51 +0800

    增加一个自动连接所有的点的模板

--- 9.8 Random Circles with No Overlap demo/sketch.js
+++ 9.8 Random Circles with No Overlap demo/sketch.js
@@ -1,44 +1,58 @@
-// 实现多个元素不重叠显示，实现的原理主要是通过 保存创建时的x,y 跟所有的x,y,进行对比，使用dist() 判断距离，如果距离大于 r1+r2,则说明不重叠
+let circles = [];
 
-let circles = []; //1. 创建一个空数组
-
-function setup () {
+function setup() {
   createCanvas(640, 360);
 
-  for (let i = 0; i < 25; i++) {
-    // 2. 创建一个对象，用于存储x,y,r
+  let num = 0;
+  while (circles.length < 5000) {
+    //   for (let i = 0; i < 25; i++) {
+
     let circle = {
       x: random(width),
       y: random(height),
-      r: 32
+      r: random(1, 10)
     }
-
-    // 4. 
+    let t = false //用于设置默认圆之间不重叠
     for (let j = 0; j < circles.length; j++) {
-      let other = circles[j].x;
-      let d = dist(circle.x, circle.y, other.x, other.y)
+      let other = circles[j]; // other 获取其他的圆 获取其他圆的x,y,r
+      let d = dist(circle.x, circle.y, other.x, other.y); // dist() 获取当前圆与其他圆的距离
+
+      if (d < circle.r + other.r) {
+        // 判断当前圆是否小于其他圆的距离小于两个圆的半径之和，如果小于，则是重叠的。只要有一个圆是重叠的，就可以跳出这个判断，并且将之前用于判断的 t 设置为 true
+        t = true
+        break;
+      }
     }
 
-    // 3. 将创建的每一个对象传入到数组中
-    circles.push(circle)
+    // 判断t 为 false 时，将合格的不重叠的点，添加到circles的数组中。
+    if (!t) {
+      circles.push(circle)
+    }
 
     // let x = random(width);
-    // let y = random(height);
+    // let y = random(height)
     // let r = 32;
+
+
     // fill(255, 0, 150, 100)
     // noStroke();
     // ellipse(x, y, r * 2, r * 2)
 
-    // 
-    for (let i = 0; i < circles.length; i++) {
-      fill(255, 0, 150, 60)
-      noStroke();
-      ellipse(circles[i].x, circles[i].y, circles[i].r * 2, circles[i].r * 2)
-
+    num++;
+    if (num > 30000) {
+      break
     }
+    // 由于不知道循环次数，所以使用了 while 循环，但是while 循环很危险，所以增加了一个保护跳出的判断
+  }
+
+  //for 循环将数组展示到画布上去
+  for (let i = 0; i < circles.length; i++) {
+    fill(255, 0, 150, 100)
+    noStroke();
+    ellipse(circles[i].x, circles[i].y, circles[i].r * 2, circles[i].r * 2)
   }
 }
 
-function draw () {
+function draw() {
   // background(220);
 }
