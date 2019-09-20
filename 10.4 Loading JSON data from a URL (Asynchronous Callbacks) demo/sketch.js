16d6a6f9d554b69d743f63cce254e0edb4664ae6
zx122248006 <zx122248006@qq.com>
2019-09-20 17:23:00 +0800

    使用randomSeed 不改变 画布中的内容

--- 10.4 Loading JSON data from a URL (Asynchronous Callbacks) demo/sketch.js
+++ 10.4 Loading JSON data from a URL (Asynchronous Callbacks) demo/sketch.js
@@ -1,21 +1,33 @@
 // function preload(){
 //   data =loadJSON('./birds.json')
 // }
-let x =0;
+let x = 0;
+let spaceData;
 
 function setup () {
   createCanvas(400, 400);
-  loadJSON('http://api.open-notify.org/astros.json', gotData, 'jsonp')
+  loadJSON('http://api.open-notify.org/astros.json', gotData, 'json')
 }
 function gotData (data) {
   console.log(data)
-
+  spaceData = data;
 }
 
 function draw () {
   background(0);
-  for (let i = 0; i < data.number; i++) {
-    fill(255)
-    ellipse(random(width), random(height), 16, 16)
+  // 在draw() 触发时，loadJSON()还没有获取到数据，所以需要增加判断
+  if (spaceData) {
+    randomSeed(4) // 可以使用randomSeed() 使得下面的random()不会随意改变
+    for (let i = 0; i < spaceData.number; i++) {
+      fill(255)
+      ellipse(random(width), random(height), 16, 16)
+    }
+  }
+
+  stroke(255)
+  line(x, 0, x, height)
+  x++;
+  if (x > width) {
+    x = 0
   }
 }
